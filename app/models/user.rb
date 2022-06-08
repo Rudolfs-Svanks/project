470c897a6e2ce5bd0c0fa54886360cd28d473dab
Rudolfs Edgars Svanks <rudolfsedgarssvanks@Admins-MacBook-Pro.local>
2022-06-08 16:21:17 +0300

    login done also added a home page

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
   include Clearance::User
 
-  include Clearance::User
+  validates :username, presence: true, uniqueness: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_06_08_100045) do
+ActiveRecord::Schema[7.0].define(version: 2022_06_08_114323) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -21,8 +21,10 @@ ActiveRecord::Schema[7.0].define(version: 2022_06_08_100045) do
     t.string "encrypted_password", limit: 128, null: false
     t.string "confirmation_token", limit: 128
     t.string "remember_token", limit: 128, null: false
+    t.string "username"
     t.index ["email"], name: "index_users_on_email"
     t.index ["remember_token"], name: "index_users_on_remember_token"
+    t.index ["username"], name: "index_users_on_username"
   end
 
 end
